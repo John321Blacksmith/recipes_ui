aa9d8110f63e782f6830dee0266b22c611b92941
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-05 16:08:21 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { MainPage } from './pages/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<MainPage/>}/>
+                <Route path='recipes/recipes/:pk' element={<RecipeDetailPage/>}/>
+                <Route path='recipes/:category' element={<CategoryRecipesPage/>}/>
+                <Route path='/:pk' element={<UserProfilePage/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App;
---
+++ src/pages/RecipeDetail.tsx
@@ -0,0 +1,6 @@
+export default function RecipeDetailPage(){
+    return (
+        <>
+        </>
+    )
+}
