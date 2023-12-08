98daef64564586e40d84a5219db3d5ace55672e4
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-08 17:07:36 +0300

    little addons

--- src/pages/CategoryRecipe.tsx
+++ src/pages/CategoryRecipe.tsx
@@ -1,13 +1,20 @@
-<<<<<<< HEAD
-export default function CategoryRecipePage(){
-=======
+
 import React, { useState, useEffect } from 'react';
+import { useGetCategoryRecipeQuery } from '../store/api/recipes'
 
 
-export function CategoryRecipesPage(){
->>>>>>> beta
+export default function CategoryRecipesPage(){
+    const {data} = useGetCategoryRecipeQuery('meat')
+    
     return (
         <>
+            {data?.map((recipe)=>{
+                return (
+                    <>
+                        <p>{JSON.stringify(recipe)}</p>
+                    </>
+                )
+            })}
         </>
     )
 }
